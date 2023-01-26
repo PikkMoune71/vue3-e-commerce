fdcd7498c651a5fa18a3d60995739b7f7f8f5c42
PikkMoune71 <antoinepicard385@gmail.com>
2023-01-26 09:38:43 +0100

    Initialized project

---
+++ src/components/AppNav.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="nav">
+    <RouterLink class="nav-item" :to="{ name: 'home' }">Home</RouterLink>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.nav {
+  background: #333;
+  color: #fff;
+  position: fixed;
+  z-index: 1000;
+  inset: 0 0 auto 0;
+  padding: 0.5rem;
+  display: flex;
+
+  &-item {
+    display: block;
+    color: inherit;
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+    &.is-active {
+      font-weight: bold;
+      color: yellow;
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>HomeView</h1>
+  </div>
 </template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
